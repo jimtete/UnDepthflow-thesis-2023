c371e0920aee3757ec325e622cc80bbe3bb81ef9
jimtete <tetepoulidis@ieee.org>
2023-02-28 10:54:30 +0200

    Update test.py

--- test.py
+++ test.py
@@ -208,6 +208,30 @@ def load_gt_disp_kitti(path, eval_occ):
         gt_disparities.append(disp)
     return gt_disparities
 
+def convert_disps_to_depths_kitti(gt_disparities, pred_disparities):
+    gt_depths = []
+    pred_depths = []
+    pred_disparities_resized = []
+
+    for i in range(len(gt_disparities)):
+        gt_disp = gt_disparities[i]
+        height, width = gt_disp.shape
+
+        pred_disp = pred_disparities[i]
+        pred_disp = width * cv2.resize(
+            pred_disp, (width, height), interpolation=cv2.INTER_LINEAR)
+
+        pred_disparities_resized.append(pred_disp)
+
+        mask = gt_disp > 0
+
+        gt_depth = width_to_focal[width] * 0.54 / (gt_disp + (1.0 - mask))
+        pred_depth = width_to_focal[width] * 0.54 / pred_disp
+
+        gt_depths.append(gt_depth)
+        pred_depths.append(pred_depth)
+    return gt_depths, pred_depths, pred_disparities_resized
+
 def compute_errors(gt, pred):
     thresh = np.maximum((gt / pred), (pred / gt))
     a1 = (thresh < 1.25).mean()

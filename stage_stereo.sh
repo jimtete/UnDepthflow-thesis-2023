f4cbdb750a30a248b9f149ab366d664ef1916d2a
jimtete <tetepoulidis@ieee.org>
2023-03-03 10:21:33 +0200

    Create stage_stereo.sh

---
+++ stage_stereo.sh
@@ -0,0 +1,1 @@
+python main.py --data_dir=../KITTI/raw --batch_size=4 --mode=stereo --train_test=train  --retrain=True  --train_file=./filenames/kitti_train_files_png_4frames.txt --gt_2012_dir=../KITTI/2012--gt_2015_dir=./KITTI/2015 --trace=./Model_Logs

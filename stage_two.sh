bda4ccf1ec14ea034d396240f0f9e6161816c561
jimtete <tetepoulidis@ieee.org>
2023-03-05 09:35:31 +0000

    did it run?

--- stage_two.sh
+++ stage_two.sh
@@ -1,2 +1,2 @@
 #!/bin/bash/bin/bash
-python main.py --data_dir=../KITTI/raw/ --batch_size=4  --mode=depth --train_test=train  --retrain=True  --train_file=./filenames/kitti_train_files_png_4frames.txt --gt_2012_dir=../KITTI/2012/training/ --gt_2015_dir=../KITTI/2015/training/ --pretrained_model=./Model_Logs/checkpoint  --trace=./Model_Logs
+python main.py --data_dir=../KITTI/raw/ --batch_size=4  --mode=depth --train_test=train  --retrain=True  --train_file=./filenames/kitti_train_files_png_4frames.txt --gt_2012_dir=../KITTI/2012/training/ --gt_2015_dir=../KITTI/2015/training/ --pretrained_model=./Model_Logs/model-3.data-00000-of-00001  --trace=./Model_Logs

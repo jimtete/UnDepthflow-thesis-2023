5e392a1f91dd904fc15072410cef6328273d5f05
jimtete <tetepoulidis@ieee.org>
2023-04-19 15:20:26 +0300

    Update test.py

--- test.py
+++ test.py
@@ -131,7 +131,7 @@ def test(sess, eval_model, itr, gt_flows_2012, noc_masks_2012, gt_flows_2015,
                 # im.save("original"+str(i)+".jpeg")
 
                 print(pred_disp.shape)
-                im = Image.fromarray(pred_disp)
+                im = Image.fromarray(255*np.squeeze(pred_disp))
                 im.save(os.path.join('50003_', str(i).zfill(6), '_pred.jpeg'))
 
                 if (custom == 1):

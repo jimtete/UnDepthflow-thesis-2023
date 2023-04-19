a5939e507ad81bef85c17af76a223591cc5745b3
jimtete <tetepoulidis@ieee.org>
2023-04-19 18:15:31 +0000

    pls work

--- test.py
+++ test.py
@@ -130,7 +130,8 @@ def test(sess, eval_model, itr, gt_flows_2012, noc_masks_2012, gt_flows_2015,
                 # im.save("custom"+str(i)+".jpeg")
                 # im.save("original"+str(i)+".jpeg")
 
-                print(pred_disp.shape)
+                np.savetxt('pls_work'+str(i).zfill(6),255*np.squeeze(pred_disp),delimiter=',')
+                print(pred_disp)
                 im = Image.fromarray(255*np.squeeze(pred_disp))
                 im =  im.convert('RGB')
                 im.save('50003_'+str(i).zfill(6)+ '_pred.jpeg')

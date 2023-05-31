360b75a18673fea00b57921ae6ce9fa665d5ea38
Dimitris Tetepoulidis <tetepoulidis@ieee.org>
2023-05-31 13:06:14 +0300

    Update test.py

--- test.py
+++ test.py
@@ -17,10 +17,19 @@ import sys
 from tensorflow.python.platform import flags
 opt = flags.FLAGS
 
+class model_type(enum):
+    hitnet = -1
+    undepthflow = -2
+
 
 def test(sess, eval_model, itr, gt_flows_2012, noc_masks_2012, gt_flows_2015,
          noc_masks_2015, gt_masks):
+
+    # Setup evaluation pipeline.
     custom = 0
+    model = model_type.hitnet
+
+
     print('Entered test file')
     with tf.name_scope("evaluation"):
         sys.stderr.write("Evaluation at iter [" + str(itr) + "]: \n")
@@ -55,114 +64,132 @@ def test(sess, eval_model, itr, gt_flows_2012, noc_masks_2012, gt_flows_2015,
                 if (total_img_num==200 and i==99):
                     continue
 
-                if (custom == 1):
-                    img1 = sm.imread("./my_pics2/000_left.jpeg")
-                    img1_orig = img1
-                    orig_H, orig_W = img1.shape[0:2]
-                    img1 = sm.imresize(img1, (opt.img_height, opt.img_width))
+                if (model == model_type.undepthflow):
+                    if (custom == 1):
+                        img1 = sm.imread("./my_pics2/000_left.jpeg")
+                        img1_orig = img1
+                        orig_H, orig_W = img1.shape[0:2]
+                        img1 = sm.imresize(img1, (opt.img_height, opt.img_width))
 
-                    img2 = sm.imread("./my_pics/001_left.jpeg")
-                    img2 = sm.imresize(img2, (opt.img_height, opt.img_width))
+                        img2 = sm.imread("./my_pics/001_left.jpeg")
+                        img2 = sm.imresize(img2, (opt.img_height, opt.img_width))
 
-                    imgr = sm.imread("./my_pics/000_right.jpeg")
-                    imgr = sm.imresize(imgr, (opt.img_height, opt.img_width))
+                        imgr = sm.imread("./my_pics/000_right.jpeg")
+                        imgr = sm.imresize(imgr, (opt.img_height, opt.img_width))
 
-                    img2r = sm.imread("./my_pics/001_right.jpeg")
-                    img2r = sm.imresize(img2r, (opt.img_height, opt.img_width))
+                        img2r = sm.imread("./my_pics/001_right.jpeg")
+                        img2r = sm.imresize(img2r, (opt.img_height, opt.img_width))
+                    else:
+                        img1 = sm.imread(
+                            os.path.join(gt_dir, "image_2",
+                                         str(i).zfill(6) + "_10.png"))
+                        img1_orig = img1
+                        orig_H, orig_W = img1.shape[0:2]
+                        img1 = sm.imresize(img1, (opt.img_height, opt.img_width))
+
+                        img2 = sm.imread(
+                            os.path.join(gt_dir, "image_2",
+                                         str(i).zfill(6) + "_11.png"))
+                        img2 = sm.imresize(img2, (opt.img_height, opt.img_width))
+
+                        imgr = sm.imread(
+                            os.path.join(gt_dir, "image_3",
+                                         str(i).zfill(6) + "_10.png"))
+
+                        imgr = sm.imresize(imgr, (opt.img_height, opt.img_width))
+
+                        img2r = sm.imread(
+                            os.path.join(gt_dir, "image_3",
+                                         str(i).zfill(6) + "_11.png"))
+                        img2r = sm.imresize(img2r, (opt.img_height, opt.img_width))
+
+                    img1 = np.expand_dims(img1, axis=0)
+                    img2 = np.expand_dims(img2, axis=0)
+                    imgr = np.expand_dims(imgr, axis=0)
+                    img2r = np.expand_dims(img2r, axis=0)
+
+                    calib_file = os.path.join(gt_dir, "calib_cam_to_cam",
+                                              str(i).zfill(6) + ".txt")
+
+                    input_intrinsic = get_scaled_intrinsic_matrix(
+                        calib_file,
+                        zoom_x=1.0 * opt.img_width / orig_W,
+                        zoom_y=1.0 * opt.img_height / orig_H)
+
+                    pred_flow_rigid, pred_flow_optical, \
+                    pred_disp, pred_disp2, pred_mask= sess.run([eval_model.pred_flow_rigid,
+                                                             eval_model.pred_flow_optical,
+                                                             eval_model.pred_disp,
+                                                             eval_model.pred_disp2,
+                                                             eval_model.pred_mask],
+                                                              feed_dict = {eval_model.input_1: img1,
+                                                                           eval_model.input_2: img2,
+                                                                           eval_model.input_r: imgr,
+                                                                           eval_model.input_2r:img2r,
+                                                                           eval_model.input_intrinsic: input_intrinsic})
+
+                    # im = Image.fromarray(255 * np.squeeze(pred_disp))
+                    # print(np.squeeze(pred_disp).shape)
+                    # im.save("custom"+str(i)+".jpeg")
+                    # im.save("original"+str(i)+".jpeg")
+
+                    # np.savetxt('pls_work'+str(i).zfill(6),2550*np.squeeze(pred_disp),delimiter=',')
+                    # print(pred_disp)
+                    # im = Image.fromarray(255*np.squeeze(pred_disp))
+                    # im =  im.convert('RGB')
+                    # im.save('50003_'+str(i).zfill(6)+ '_pred.jpeg')
+
+                    image_to_print = 2  # 1 for original, 2 for depth pred.
+                    if (image_to_print == 1):
+                        pred = img1_orig
+                        mode_img = 'RGB'
+                    else:
+                        stage_zero = pred_disp
+                        mode_img = 'L'
+                        stage_one = np.squeeze(stage_zero)
+                        stage_two = stage_one * 255
+                        max = np.max(stage_two)
+                        stage_three = stage_two * (255 / max)
+                        pred = stage_three.astype(np.uint8)
+
+                    print(pred.shape)
+                    im = Image.fromarray(pred, mode=mode_img).convert('RGB')
+
+                    if eval_data == "kitti_2012":
+                        title = 'kitti_2012_65003_' + str(i).zfill(6) + '_pred.jpeg'
+                    else:
+                        title = 'kitti_2015_65003_' + str(i).zfill(6) + '_pred.jpeg'
+
+                    print("saved image: " + title)
+                    im.save(title)
+
+                    if (custom == 1):
+                        print("Press Ctrl + C")
+                        continue
+
+                    test_result_flow_rigid.append(np.squeeze(pred_flow_rigid))
+                    test_result_flow_optical.append(np.squeeze(pred_flow_optical))
+                    test_result_disp.append(np.squeeze(pred_disp))
+                    test_result_disp2.append(np.squeeze(pred_disp2))
+                    test_result_mask.append(np.squeeze(pred_mask))
+                    test_image1.append(img1_orig)
+
+                if (eval_data == "kitti_2012"):
+                    eval_kitti_year = "2012"
                 else:
-                    img1 = sm.imread(
-                        os.path.join(gt_dir, "image_2",
-                                     str(i).zfill(6) + "_10.png"))
-                    img1_orig = img1
-                    orig_H, orig_W = img1.shape[0:2]
-                    img1 = sm.imresize(img1, (opt.img_height, opt.img_width))
+                    eval_kitti_year = "2015"
 
-                    img2 = sm.imread(
-                        os.path.join(gt_dir, "image_2",
-                                     str(i).zfill(6) + "_11.png"))
-                    img2 = sm.imresize(img2, (opt.img_height, opt.img_width))
+                if (model == model_type.hitnet):
+                    img_path = "predictions/hitnet/training/"+eval_kitti_year+"/"+str(i).zfill(6) + ".png"
+                    img = sm.imread(img_path)
+                    print(np.array(img).shape)
 
-                    imgr = sm.imread(
-                        os.path.join(gt_dir, "image_3",
-                                     str(i).zfill(6) + "_10.png"))
 
-                    imgr = sm.imresize(imgr, (opt.img_height, opt.img_width))
+                # Check np shape
+                # Check array values
 
-                    img2r = sm.imread(
-                        os.path.join(gt_dir, "image_3",
-                                     str(i).zfill(6) + "_11.png"))
-                    img2r = sm.imresize(img2r, (opt.img_height, opt.img_width))
 
-                img1 = np.expand_dims(img1, axis=0)
-                img2 = np.expand_dims(img2, axis=0)
-                imgr = np.expand_dims(imgr, axis=0)
-                img2r = np.expand_dims(img2r, axis=0)
 
-                calib_file = os.path.join(gt_dir, "calib_cam_to_cam",
-                                          str(i).zfill(6) + ".txt")
-
-                input_intrinsic = get_scaled_intrinsic_matrix(
-                    calib_file,
-                    zoom_x=1.0 * opt.img_width / orig_W,
-                    zoom_y=1.0 * opt.img_height / orig_H)
-
-                pred_flow_rigid, pred_flow_optical, \
-                pred_disp, pred_disp2, pred_mask= sess.run([eval_model.pred_flow_rigid,
-                                                         eval_model.pred_flow_optical,
-                                                         eval_model.pred_disp,
-                                                         eval_model.pred_disp2,
-                                                         eval_model.pred_mask],
-                                                          feed_dict = {eval_model.input_1: img1,
-                                                                       eval_model.input_2: img2,
-                                                                       eval_model.input_r: imgr,
-                                                                       eval_model.input_2r:img2r,
-                                                                       eval_model.input_intrinsic: input_intrinsic})
-
-                test_result_flow_rigid.append(np.squeeze(pred_flow_rigid))
-                test_result_flow_optical.append(np.squeeze(pred_flow_optical))
-                test_result_disp.append(np.squeeze(pred_disp))
-                test_result_disp2.append(np.squeeze(pred_disp2))
-                test_result_mask.append(np.squeeze(pred_mask))
-                test_image1.append(img1_orig)
-
-                #im = Image.fromarray(255 * np.squeeze(pred_disp))
-                #print(np.squeeze(pred_disp).shape)
-                # im.save("custom"+str(i)+".jpeg")
-                # im.save("original"+str(i)+".jpeg")
-
-                # np.savetxt('pls_work'+str(i).zfill(6),2550*np.squeeze(pred_disp),delimiter=',')
-                # print(pred_disp)
-                # im = Image.fromarray(255*np.squeeze(pred_disp))
-                # im =  im.convert('RGB')
-                # im.save('50003_'+str(i).zfill(6)+ '_pred.jpeg')
-
-                image_to_print = 2 # 1 for original, 2 for depth pred.
-                if (image_to_print == 1):
-                    pred = img1_orig
-                    mode_img = 'RGB'
-                else:
-                    stage_zero = pred_disp
-                    mode_img = 'L'
-                    stage_one = np.squeeze(stage_zero)
-                    stage_two = stage_one * 255
-                    max = np.max(stage_two)
-                    stage_three = stage_two * (255/max)
-                    pred = stage_three.astype(np.uint8)
-
-                print(pred.shape)
-                im = Image.fromarray(pred, mode=mode_img).convert('RGB')
-
-                if eval_data == "kitti_2012":
-                    title = 'kitti_2012_65003_'+str(i).zfill(6)+ '_pred.jpeg'
-                else:
-                    title = 'kitti_2015_65003_' + str(i).zfill(6) + '_pred.jpeg'
-
-                print("saved image: "+title)
-                im.save(title)
-
-                if (custom == 1):
-                    print("Press Ctrl + C")
-                    continue
 
             ## depth evaluation
             if opt.eval_depth and eval_data == "kitti_2015":

6d867dcb4e124223034599cb4fb0f55846776985
jimtete <tetepoulidis@ieee.org>
2023-06-14 09:51:30 +0300

    preparing for training

--- test.py
+++ test.py
@@ -28,7 +28,7 @@ def test(sess, eval_model, itr, gt_flows_2012, noc_masks_2012, gt_flows_2015,
 
     # Setup evaluation pipeline.
     custom = 0
-    model = model_type.hitnet
+    model = model_type.undepthflow
 
 
     print('Entered test file')
@@ -140,33 +140,36 @@ def test(sess, eval_model, itr, gt_flows_2012, noc_masks_2012, gt_flows_2015,
                     # im =  im.convert('RGB')
                     # im.save('50003_'+str(i).zfill(6)+ '_pred.jpeg')
 
-                    image_to_print = 2  # 1 for original, 2 for depth pred.
-                    if (image_to_print == 1):
-                        pred = img1_orig
-                        mode_img = 'RGB'
-                    else:
-                        stage_zero = pred_disp
-                        mode_img = 'L'
-                        stage_one = np.squeeze(stage_zero)
-                        stage_two = stage_one * 255
-                        max = np.max(stage_two)
-                        stage_three = stage_two * (255 / max)
-                        pred = stage_three.astype(np.uint8)
+                    # TODO: CHECK HERE
+                    # That's for printing the image
 
-                    print(pred.shape)
-                    im = Image.fromarray(pred, mode=mode_img).convert('RGB')
-
-                    if eval_data == "kitti_2012":
-                        title = 'kitti_2012_65003_' + str(i).zfill(6) + '_pred.jpeg'
-                    else:
-                        title = 'kitti_2015_65003_' + str(i).zfill(6) + '_pred.jpeg'
-
-                    print("saved image: " + title)
-                    im.save(title)
-
-                    if (custom == 1):
-                        print("Press Ctrl + C")
-                        continue
+                    # image_to_print = 2  # 1 for original, 2 for depth pred.
+                    # if (image_to_print == 1):
+                    #     pred = img1_orig
+                    #     mode_img = 'RGB'
+                    # else:
+                    #     stage_zero = pred_disp
+                    #     mode_img = 'L'
+                    #     stage_one = np.squeeze(stage_zero)
+                    #     stage_two = stage_one * 255
+                    #     max = np.max(stage_two)
+                    #     stage_three = stage_two * (255 / max)
+                    #     pred = stage_three.astype(np.uint8)
+                    #
+                    # print(pred.shape)
+                    # im = Image.fromarray(pred, mode=mode_img).convert('RGB')
+                    #
+                    # if eval_data == "kitti_2012":
+                    #     title = 'kitti_2012_65003_' + str(i).zfill(6) + '_pred.jpeg'
+                    # else:
+                    #     title = 'kitti_2015_65003_' + str(i).zfill(6) + '_pred.jpeg'
+                    #
+                    # print("saved image: " + title)
+                    # im.save(title)
+                    #
+                    # if (custom == 1):
+                    #     print("Press Ctrl + C")
+                    #     continue
 
                     test_result_flow_rigid.append(np.squeeze(pred_flow_rigid))
                     test_result_flow_optical.append(np.squeeze(pred_flow_optical))
